9cd27917661f6dff42925185f77775e65a59ac59
LegacyWoW <mayormccyt@gmail.com>
2026-01-19 14:15:55 -0800

    Update app.js

--- map/app.js
+++ map/app.js
@@ -189,4 +189,33 @@ function applyStyle(layer) {
     layer.setStyle({
       color,
       fillColor: color,
-      fil
+      fillOpacity: 0.25,
+      opacity: 0.9,
+      weight: 3
+    });
+  }
+}
+
+// ---- UTILITIES ----
+function downloadJSON(obj, filename) {
+  const blob = new Blob([JSON.stringify(obj, null, 2)], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+  URL.revokeObjectURL(url);
+}
+
+function escapeHtml(s) {
+  return String(s).replace(/[&<>"']/g, c => ({
+    "&":"&amp;",
+    "<":"&lt;",
+    ">":"&gt;",
+    "\"":"&quot;",
+    "'":"&#39;"
+  }[c]));
+}
+function escapeAttr(s){ return escapeHtml(s).replace(/"/g,"&quot;"); }
